05c00552fd7e075c9333d1566f84a7cee2b03488
dhyeys249 <dhyey@desireinfoweb.com>
2023-03-31 19:34:28 +0530

    commit

--- Custom_Calendar/src/webparts/calendar/components/ICalendarProps.ts
+++ Custom_Calendar/src/webparts/calendar/components/ICalendarProps.ts
@@ -1,13 +1,20 @@
-import { DisplayMode } from '@microsoft/sp-core-library';
+import { DisplayMode } from "@microsoft/sp-core-library";
 import { WebPartContext } from "@microsoft/sp-webpart-base";
-import { IDateTimeFieldValue } from '@pnp/spfx-property-controls/lib/PropertyFieldDateTimePicker';
+import { IDateTimeFieldValue } from "@pnp/spfx-property-controls/lib/PropertyFieldDateTimePicker";
 export interface ICalendarProps {
-  title: string;
+  pTitle: string;
   siteUrl: string;
   list: string;
-  displayMode: DisplayMode;
-  updateProperty: (value: string) => void;
-  context: WebPartContext;
-  eventStartDate:  IDateTimeFieldValue;
-  eventEndDate: IDateTimeFieldValue;
+  pDisplayMode: DisplayMode;
+  // eventStartDate: IDateTimeFieldValue;
+  // eventEndDate: IDateTimeFieldValue;
+  pSpfxContext: WebPartContext;
+  userDisplayName: string;
+  pUpdateProperty: (value: string) => void;
+  pContext: WebPartContext;
+  context: any;
+  checkPermission: any;
+  uploadImage: any;
+  backToHome: any;
+  SiteUrl: any;
 }
